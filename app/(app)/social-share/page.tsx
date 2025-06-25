01798e19fa32f64067064bcf02269c855028ac5b
Raja Kumar <rajaraja360parth@gmail.com>
2025-06-25 16:29:16 +0530

    connected backend and added some page

---
+++ app/(app)/social-share/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SocialShare(props) {
+    return (
+        <div>
+            Socialshare
+        </div>
+    );
+}
+
+export default SocialShare;
---
+++ app/(app)/video-upload/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function VideoUpload() {
+  return (
+    <div>
+      <h1>Video Upload</h1>
+      <p>Upload your video here.</p>
+      {/* Add your video upload form or component here */}
+    </div>
+  );
+}
+
+export default VideoUpload;
